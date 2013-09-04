7ee6d3a01839bd93b4d7e1bc9c6bc8b54fc77ae6
tungul <lugnut1206@gmail.com>
2013-09-03 22:47:28 -0700

    cleanliness for me

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+echo "> git pull"
 git pull
+echo "remove old zip"
 rm GameMode_Microlite20.zip
+echo "create new zip"
 zip GameMode_Microlite20.zip *
 zip -d GameMode_Microlite20.zip build.sh README.md # exclude these files from finished add-on
+echo "git add -A"
 git add -A
+echo "commit"
 git commit
+echo "push"
 git push
+echo "done"
