e155680c928216c03d636361160e25bce044a5b0
Tungul <lugnut1206@gmail.com>
2013-09-04 01:10:34 -0700

    fixed continuity

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ zip GameMode_Microlite20.zip *
 zip -d GameMode_Microlite20.zip build.sh README.md # exclude these files from finished add-on
 echo "--- Pushing to server"
 echo "cd Add-Ons ; put GameMode_Microlite20.zip" | ftp hammereditor.net
-echo "git add -A"
+echo "--- git add -A"
 git add -A
 echo "--- Commit"
 git commit
-echo "Push to github"
+echo "--- Push to github"
 git push
-echo "Complete"
+echo "--- Complete"
