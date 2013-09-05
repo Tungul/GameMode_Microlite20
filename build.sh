a8eff85e7405ad2d264a19fa6e7fb41fa9674d76
tungul <lugnut1206@gmail.com>
2013-09-04 23:39:18 -0700

    just a minor note in build.sh, after like 6 revisions it finally fucking worked

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ zip GameMode_Microlite20.zip *
 zip -d GameMode_Microlite20.zip build.sh README.md # exclude these files from finished add-on
 mkdir Add-Ons; cp GameMode_Microlite20.zip Add-Ons
 echo "--- Pushing to server"
-echo "put Add-Ons/*" | ftp hammereditor.net && echo "--- FTP push success"
+echo "put Add-Ons/*" | ftp hammereditor.net && echo "--- FTP push success" # just making a minor note here: OH MY FUCKING GOD IT WORKED FINALLY
 rm -rf Add-Ons/
 echo "--- git add -A"
 git add -A
