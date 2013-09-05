ea06f9f074927be3032033896116956f3223851c
Tungul <lugnut1206@gmail.com>
2013-09-05 16:03:59 -0700

    that wouldn't've worked.

--- lib/centerprint_scroller.cs
+++ lib/centerprint_scroller.cs
@@ -30,9 +30,9 @@ package CenterprintTextScroller {
 	}
 	function serverCmdClearGhostBrick(%client) {
 		if(%client.inScrollableListMode) {
-			messageClient(%client, '', "\c5To scroll line by line, use your shift brick up/down 1/3 keys.");
-			messageClient(%client, '', "\c5To scroll page by page, use your normal shift brick up/down keys.");
-			messageClient(%client, '', "\c5To exit, use your clear ghost brick key.");
+			messageClient(%client, '', "\c5To scroll by line, use your shift brick up/down 1/3 keys.");
+			messageClient(%client, '', "\c5To scroll by page, use your normal shift brick up/down keys.");
+			messageClient(%client, '', "\c5To exit, use your plant brick key.");
 			
 		}
 	}
@@ -71,10 +71,10 @@ function ScrollerObject::printLoop(%this, %on) {
 
 	%this.printLoopSchedule = %this.schedule(250, printLoop, 1);
 
+	%data = "Push your clear ghost brick key for help.";
 	for(%i = %this.headLine; %i < %this.linesShown; %i++) {
 		%data = %data @ %this.lines[i];
 	}
-	%data = %data @ "Push your clear ghost brick key for help.";
 	%this.client.centerPrint(%data);
 }
 
