88153cddebfe8b488db92e5ea824cf43cd501684
tungul <lugnut1206@gmail.com>
2013-09-06 20:47:04 -0700

    update zip and remove faulty ftp stuff

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ rm GameMode_Microlite20.zip
 echo "--- Build new zip"
 zip GameMode_Microlite20.zip *
 zip -d GameMode_Microlite20.zip build.sh README.md # exclude these files from finished add-on
-mkdir Add-Ons; cp GameMode_Microlite20.zip Add-Ons
-echo "--- Pushing to server"
-echo "put Add-Ons/*" | ftp hammereditor.net && echo "--- FTP push success" # just making a minor note here: OH MY FUCKING GOD IT WORKED FINALLY
-rm -rf Add-Ons/
+# mkdir Add-Ons; cp GameMode_Microlite20.zip Add-Ons
+# echo "--- Pushing to server"
+# echo "put Add-Ons/*" | ftp hammereditor.net && echo "--- FTP push success" # just making a minor note here: OH MY FUCKING GOD IT WORKED FINALLY
+# rm -rf Add-Ons/
 echo "--- git add -A"
 git add -A
 echo "--- Commit"
